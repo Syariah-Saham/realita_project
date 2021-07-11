53b8cb6f54b21748be5ce0920ab3a566ea31a36c
BJ-stack-art <bj.angel119@gmail.com>
2021-07-11 21:09:09 +0700

    update link package in dashboard admin

--- resources/views/vendor/admin/dashboard.blade.php
+++ resources/views/vendor/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
 <div class="px-1 md:px-6 lg:px-8 my-4 mb-8">
 	<h1 class="text-2xl font-bold text-gray-600 mb-2 mt-6">Dashboard</h1>
 	<div class="flex flex-col md:flex-row gap-8 mt-4 mb-8">
-        <a href="{{ url('admin/package') }}" class="w-full transition duration-300 transform hover:translate-y-1 ease-in-out md:w-1/4">
+        <a href="{{ url('admin/package/5') }}" class="w-full transition duration-300 transform hover:translate-y-1 ease-in-out md:w-1/4">
             <div style="background-image: linear-gradient(136deg , #de11fd 0%, #35e49c 100%);" class="bg-gr shadow-md rounded-3xl flex flex-row items-center p-3 justify-between  text-white gap-3">
                 <svg class="w-2/5 max-h-20 justify-center"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
@@ -17,7 +17,7 @@
                 </div>
             </div>
         </a>
-        <a href="{{ url('admin/package') }}" class="w-full transition duration-300 transform hover:translate-y-1 ease-in-out md:w-1/4">
+        <a href="{{ url('admin/package/3') }}" class="w-full transition duration-300 transform hover:translate-y-1 ease-in-out md:w-1/4">
             <div style="background-image: linear-gradient(136deg, #2af598 0%, #009efd 100%);" class="bg-gr shadow-md rounded-3xl flex flex-row items-center p-3 justify-between  text-white gap-3">
                 <svg class="w-2/5 max-h-20 justify-center"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
@@ -28,7 +28,7 @@
                 </div>
             </div>
         </a>
-		<a href="{{ url('admin/package') }}" class="w-full transition duration-300 transform hover:translate-y-1 ease-in-out md:w-1/4">
+		<a href="{{ url('admin/package/4') }}" class="w-full transition duration-300 transform hover:translate-y-1 ease-in-out md:w-1/4">
 			<div style="background-image: linear-gradient(136deg, #009efd 0%, #f900fd 100%);" class="bg-gr shadow-md rounded-3xl flex flex-row items-center p-3 justify-between  text-white gap-3">
                 <svg class="w-2/5 max-h-20 justify-center"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
