4a8ff1e9d76d542ae14e5548382d4eb798d09872
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-07-24 12:22:17 +0700

    fix view admmin.report-create.blade.php

--- resources/views/vendor/admin/report-create.blade.php
+++ resources/views/vendor/admin/report-create.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.admin')
 @section('title'  , 'Buat Laporan Keuangan')
 @section('body')
+@include('vendor.components.modal-delete')
 
 <div class="flex flex-row gap-4 pb-20">
 	<form action="{{ url('/admin/report/create') }}" method="post" class="w-2/3">
@@ -240,15 +241,11 @@
 					<tr>
 						<td class="p-2">{{ $periode->year }}</td>
 						<td class="col-badge">
-							<form action="{{ url('/admin/periode/'.$periode->id) }}" method="post">
-								@csrf
-								@method('delete')
-								<button class="badge bg-red">
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-									  <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
-									</svg>
-								</button>
-							</form>
+							<button data-action="{{ url('admin/periode/'.$periode->id) }}" class="badge badge-delete bg-red">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+								</svg>
+							</button>
 						</td>
 					</tr>
 				@empty
