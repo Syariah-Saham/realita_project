d1a950d2eaa7044bc9d935ba23f8567505e12fa7
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-09-11 20:39:40 +0700

    chore : create register method for register from frontend

---
+++ app/Http/Controllers/Api/AuthControllerr.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthControllerr extends Controller
+{
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('syariahsaham')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return $response;
+    }
+}
